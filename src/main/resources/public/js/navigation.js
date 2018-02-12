5cf7b6843e1aacbf98104e408b85e9ca4e63ddeb
keanemu <keanemu@gmail.com>
2018-02-12 09:16:02 -0500

    navigation functional

---
+++ src/main/resources/public/js/navigation.js
@@ -0,0 +1,12 @@
+$(document).ready(
+        function() {
+            var subnav = $("#subnav").attr("name");
+            var nav = $("#nav").attr("name");
+            if (subnav == "") {
+                $('#' + nav).addClass("navigation__active");
+            } else {
+                $('#' + nav).addClass(
+                        "navigation__sub--active navigation__sub--toggled");
+                $('#' + subnav).addClass("navigation__active");
+            }
+        });
