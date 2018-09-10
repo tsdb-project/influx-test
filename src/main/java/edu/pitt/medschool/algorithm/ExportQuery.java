a8da3641d791e892141dd563bfe46407708d60c5
CptTony <zyc@gis4.org>
2018-09-10 18:09:12 -0400

    Start to implement ExportQuery Class, need to make sure to understand our down-sample/aggregation.

--- src/main/java/edu/pitt/medschool/algorithm/ExportQuery.java
+++ src/main/java/edu/pitt/medschool/algorithm/ExportQuery.java
@@ -4,6 +4,7 @@ import edu.pitt.medschool.model.DataTimeSpanBean;
 import edu.pitt.medschool.model.dto.DownsampleGroup;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Queries for doing the downsample-aggregation query
@@ -19,27 +20,73 @@ public class ExportQuery {
     // Downsample configs
     private DownsampleGroup dsgVo;
     private boolean isDownSampleFirst;
+    private List<String> columnNames;
 
     // Metadata for patients
     private String pid;
     private int numDataSegments;
     private List<DataTimeSpanBean> timeseriesMetadata;
 
+    // Final query string
+    private boolean isQueryBuilt = false;
+    private String queryString;
+
     /**
      * Initialize this class (Expection if dts null or no length)
      *
      * @param dts             Data
      * @param v               DownsampleGroupVO
+     * @param columns         Columns (e.g. I10_4,I11_4)
      * @param downSampleFirst Is Downsample first or aggregation first
      */
-    public ExportQuery(List<DataTimeSpanBean> dts, DownsampleGroup v, boolean downSampleFirst) {
+    private ExportQuery(
+            List<DataTimeSpanBean> dts,
+            DownsampleGroup v,
+            List<String> columns,
+            boolean downSampleFirst) throws IllegalArgumentException {
         initData(dts);
         this.dsgVo = v;
         this.isDownSampleFirst = downSampleFirst;
+        this.columnNames = columns;
+
+        buildQuery();
     }
 
     /**
-     * Extract from `DataTimeSpanBean` to set some info for this class
+     * Assemble the query string from given data.
+     *
+     * @param d               Data
+     * @param v               DownsampleGroupVO
+     * @param columns         Columns (e.g. I10_4,I11_4)
+     * @param downSampleFirst Is Downsample first or aggregation first
+     */
+    public static String generate(List<DataTimeSpanBean> d,
+                                  DownsampleGroup v, List<String> columns,
+                                  boolean downSampleFirst) throws Exception {
+        return new ExportQuery(d, v, columns, downSampleFirst).toString();
+    }
+
+    /**
+     * Get the final assembled queries
+     */
+    @Override
+    public String toString() {
+        if (!this.isQueryBuilt) {
+            throw new RuntimeException("Failed to build query string");
+        } else {
+            return this.queryString;
+        }
+    }
+
+    private void buildQuery() {
+        StringBuilder sb = new StringBuilder("SELECT ");
+
+        this.queryString = sb.toString();
+        this.isQueryBuilt = true;
+    }
+
+    /**
+     * Extract from `DataTimeSpanBean` to set some basic info for this class
      */
     private void initData(List<DataTimeSpanBean> dts) {
         if (dts == null || dts.isEmpty()) throw new IllegalArgumentException("DataTimeSpanBean empty");
@@ -49,10 +96,43 @@ public class ExportQuery {
     }
 
     /**
-     * Get the final assembled queries
+     * Concat the column name list into an add string: ("f1"+"f2")
+     * For aggregation
+     *
+     * @param alias Alias for this list, null for not using
      */
-    @Override
-    public String toString() {
-        return super.toString();
+    private String formColumnNamesToAddQuery(String alias) {
+        return selectQueryWithAlias(String.format("(%s)",
+                this.columnNames.stream()
+                        .map(s -> "\"" + s + "\"")
+                        .collect(Collectors.joining("+"))),
+                alias);
     }
+
+    /**
+     * Concat the column name list into an mean string": (("f1"+"f2")/2)
+     * For aggregation
+     *
+     * @param alias Alias for this list, null for not using
+     */
+    private String formColumnNamesToMeanQuery(String alias) {
+        return selectQueryWithAlias(
+                String.format("((%s)/%d)",
+                        formColumnNamesToAddQuery(null),
+                        this.columnNames.size()),
+                alias);
+    }
+
+    /**
+     * Give alias for select statements
+     * (COUNT(I10_1,I10_2) -> COUNT(I10_1,I10_2) AS A)
+     *
+     * @param origin Original select obj
+     * @param alias  Alias, null for not using
+     */
+    private String selectQueryWithAlias(String origin, String alias) {
+        if (alias == null) return origin;
+        else return String.format("%s AS %s", origin, alias);
+    }
+
 }
