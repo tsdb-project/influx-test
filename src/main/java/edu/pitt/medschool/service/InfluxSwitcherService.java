e7e7bc08fc1f7e8b8a954d019c7b8b62b5fcd7e1
CptTony <zyc@gis4.org>
2018-09-23 04:44:03 -0400

    Working: submit Influx job, tunnel the port and close it.

--- src/main/java/edu/pitt/medschool/service/InfluxSwitcherService.java
+++ src/main/java/edu/pitt/medschool/service/InfluxSwitcherService.java
@@ -1,15 +1,19 @@
 package edu.pitt.medschool.service;
 
-import com.jcraft.jsch.JSch;
-import com.jcraft.jsch.JSchException;
-import com.jcraft.jsch.Session;
+import com.jcraft.jsch.*;
 import edu.pitt.medschool.config.InfluxappConfig;
+import edu.pitt.medschool.framework.util.TimeUtil;
+import edu.pitt.medschool.framework.util.Util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Switch between PSC and local InfluxDB
@@ -18,24 +22,213 @@ import java.util.Properties;
 public class InfluxSwitcherService {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    private class Template {
+        private Template() {
+        }
+
+        static final String COMMAND_BASIC = "/bin/bash -c \"%s\"";
+        static final String NOW_PID_PATH = "/pylon5/bi5fpep/quz3/ondemand_log/nowpid";
+        static final String READ_NOW_HOST = "cat /pylon5/bi5fpep/quz3/ondemand_log/nowhost";
+    }
+
+    private boolean isSshReady = false;
     private JSch jsch = new JSch();
+    private Pattern jobIdPattern = Pattern.compile("[0-9]+");
+
+    private String currentJobId = "";
+    private String remoteInfluxHostname = "";
 
     @Value("${ssh-username}")
     private String username;
 
-    public InfluxSwitcherService() throws JSchException {
-        jsch.addIdentity(InfluxappConfig.SSH_PRIVATEKEY_PATH);
+    /**
+     * Initialize SSH client related info
+     */
+    public InfluxSwitcherService() {
+        try {
+            jsch.addIdentity(InfluxappConfig.SSH_PRIVATEKEY_PATH);
+            HostKey hk = new HostKey(InfluxappConfig.REMOTE_SSH_HOST, HostKey.SSHRSA, InfluxappConfig.BRIDGE_LOGIN_PUBKEY_RSA);
+            jsch.getHostKeyRepository().add(hk, null);
+            isSshReady = true;
+        } catch (JSchException e) {
+            logger.error("Init SSH client failed: {}", Util.stackTraceErrorToString(e));
+        }
     }
 
-    public void loginPsc() {
-
+    /**
+     * Submit start InfluxDB sbatch job on PSC
+     *
+     * @return True if command successfully inited, False if error happend
+     */
+    public boolean submitStartPscInflux() {
+        if (!this.isSshReady) return false;
+        // Avoid start one again
+        if (!this.currentJobId.isEmpty()) return false;
+        if (!this.remoteInfluxHostname.isEmpty()) return false;
+        this.currentJobId = "";
+        try {
+            Matcher m = jobIdPattern.matcher(runCommandViaSSH(String.format(Template.COMMAND_BASIC, "cd /pylon5/bi5fpep/quz3/ondemand;sbatch start-influx;")));
+            if (m.find()) {
+                this.currentJobId = m.group();
+            } else {
+                return false;
+            }
+        } catch (Exception e) {
+            logger.error("SSH batch job failed to submit: {}", Util.stackTraceErrorToString(e));
+            return false;
+        }
+        if (this.currentJobId.isEmpty()) return false;
+        logger.warn("InfluxDB start job submitted with ID <{}>.", this.currentJobId);
+        return true;
     }
 
-    private Session generateNewSshSession() throws JSchException {
+    /**
+     * Stop the InfluxDB job (Better wait 30s when to let InfluxDB stop
+     *
+     * @return True if idb stop signal successfully stopped, False if error happend
+     */
+    public boolean stopPscInflux() {
+        if (!this.isSshReady) return false;
+        // Avoid stop a not started server (Optional)
+        if (this.currentJobId.isEmpty()) return false;
+        try {
+            String res = runCommandViaSSH(String.format("srun --jobid=%s kill -15 $(cat %s);", this.currentJobId, Template.NOW_PID_PATH));
+            if (!res.trim().isEmpty()) return false;
+        } catch (Exception e) {
+            logger.error("SSH batch job failed to stop: {}", Util.stackTraceErrorToString(e));
+            return false;
+        }
+        logger.warn("InfluxDB stop signal sent for ID <{}>.", this.currentJobId);
+        this.currentJobId = "";
+        this.remoteInfluxHostname = "";
+        return true;
+    }
+
+    private Session forwardSession;
+
+    public void startPortForward() {
+        if (this.forwardSession != null) return;
+        if (this.remoteInfluxHostname.isEmpty()) {
+            tryInitRemoteInfluxHostname();
+        }
+        try {
+            forwardSession = generateNewSshSession(true);
+            forwardSession.connect();
+            forwardSession.setPortForwardingL("localhost", 8086, remoteInfluxHostname, 8086);
+            logger.warn("Port forward to <{}> started on <{}>", remoteInfluxHostname, TimeUtil.formatLocalDateTime(null, ""));
+        } catch (JSchException e) {
+            logger.error("Set port forward failed: {}", Util.stackTraceErrorToString(e));
+        }
+    }
+
+    public void stopPortForward() {
+        if (this.forwardSession == null) return;
+        try {
+            forwardSession.delPortForwardingL(8086);
+            logger.warn("Port forward to <{}> stopped on <{}>", remoteInfluxHostname, TimeUtil.formatLocalDateTime(null, ""));
+        } catch (JSchException e) {
+            logger.error("Stop port forward failed: {}", Util.stackTraceErrorToString(e));
+        } finally {
+            forwardSession.disconnect();
+            forwardSession = null;
+        }
+    }
+
+    /**
+     * Check if a InfluxDB job is in queue (wait) or started
+     */
+    public boolean pscInfluxIsInQueue() {
+        String status = "";
+        try {
+            status = runCommandViaSSH(String.format(Template.COMMAND_BASIC, Template.READ_NOW_HOST)).trim();
+        } catch (Exception e) {
+            logger.error("Failed to get status of currently running InfluxDB");
+        }
+        return status.contains("No such file or directory");
+    }
+
+    /**
+     * Try initialize the remote InfluxDB hostname
+     */
+    public void tryInitRemoteInfluxHostname() {
+        try {
+            this.remoteInfluxHostname = runCommandViaSSH(String.format(Template.COMMAND_BASIC, Template.READ_NOW_HOST)).trim();
+            if (this.remoteInfluxHostname.contains("No such file or directory")) {
+                this.remoteInfluxHostname = "";
+            }
+        } catch (Exception e) {
+            logger.error("Failed to get remote Influx hostname");
+        }
+    }
+
+    /**
+     * Usually Idb takes 5 minutes to start, we have to check it make sure it actually started
+     */
+    public boolean hasPscInfluxStarted() {
+        if (this.remoteInfluxHostname.isEmpty()) {
+            tryInitRemoteInfluxHostname();
+        }
+        String res;
+        try {
+            res = runCommandViaSSH(String.format(Template.COMMAND_BASIC, "tail -n50 /pylon5/bi5fpep/quz3/ondemand_log/*.log"));
+        } catch (Exception e) {
+            logger.error("Failed to determine InfluxDB status: {}", Util.stackTraceErrorToString(e));
+            return false;
+        }
+        return res.contains("Compacting file") || res.contains("Listening on HTTP");
+    }
+
+    /**
+     * Generate a new SSH session and run the command
+     */
+    private String runCommandViaSSH(String command) throws JSchException, IOException {
+        Session session = generateNewSshSession(true);
+        session.connect();
+
+        ChannelExec channel = (ChannelExec) session.openChannel("exec");
+        channel.setCommand(command);
+        channel.setInputStream(null);
+        channel.setOutputStream(null);
+        channel.setPty(true);
+        InputStream in = channel.getInputStream();
+        channel.connect();
+
+        StringBuilder sb = new StringBuilder(1024);
+        byte[] buffer = new byte[1024];
+        while (true) {
+            while (in.available() > 0) {
+                int i = in.read(buffer, 0, 1024);
+                if (i < 0) break;
+                sb.append(new String(buffer, 0, i));
+            }
+            if (channel.isClosed()) {
+                if (in.available() > 0) continue;
+                break;
+            }
+        }
+        logger.info("SSH command <{}>, status: {}.", command, channel.getExitStatus());
+        if (channel.getExitStatus() != 0) {
+            logger.warn("SSH command <{}> message: '{}'", command, sb.toString());
+        }
+        in.close();
+        channel.disconnect();
+        session.disconnect();
+        return sb.toString();
+    }
+
+    /**
+     * Generate a new SSH session from JSch
+     */
+    private Session generateNewSshSession(boolean needCompress) throws JSchException {
         Session session = jsch.getSession(username, InfluxappConfig.REMOTE_SSH_HOST);
-        Properties config = new Properties();
-        config.put("StrictHostKeyChecking", "no");
+        java.util.Properties config = new Properties();
+        config.put("StrictHostKeyChecking", "yes");
         config.put("PreferredAuthentications", "publickey");
+        if (needCompress) {
+            config.put("compression.s2c", "zlib,none");
+            config.put("compression.c2s", "zlib,none");
+            config.put("compression_level", "9");
+        }
         session.setConfig(config);
         return session;
     }
--- src/test/java/edu/pitt/medschool/service/sshServiceTest.java
+++ src/test/java/edu/pitt/medschool/service/sshServiceTest.java
@@ -1,18 +1,19 @@
 package edu.pitt.medschool.service;
 
-import com.jcraft.jsch.ChannelExec;
-import com.jcraft.jsch.JSch;
-import com.jcraft.jsch.JSchException;
-import com.jcraft.jsch.Session;
+import com.jcraft.jsch.*;
 import edu.pitt.medschool.config.InfluxappConfig;
 import edu.pitt.medschool.framework.util.Util;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class sshServiceTest {
@@ -20,9 +21,12 @@ public class sshServiceTest {
     private JSch jsch = new JSch();
     private Session session1, session2;
 
+    private Pattern jobIdPattern = Pattern.compile("[0-9]+");
+
     @Before
     public void initClientTest() throws JSchException {
-        // Path to private key file
+        HostKey hk = new HostKey(InfluxappConfig.REMOTE_SSH_HOST, HostKey.SSHRSA, InfluxappConfig.BRIDGE_LOGIN_PUBKEY_RSA);
+        jsch.getHostKeyRepository().add(hk, null);
         jsch.addIdentity(InfluxappConfig.SSH_PRIVATEKEY_PATH);
         session1 = generateNewSshSession();
         session2 = generateNewSshSession();
@@ -34,7 +38,7 @@ public class sshServiceTest {
         System.out.println(s1);
         assertTrue(s1.contains("pvt.bridges."));
         assertTrue(s1.contains("idb_deploy_old"));
-        String s2 = testRunner(session2, "ssh r732.pvt.bridges.psc.edu;");
+        String s2 = testRunner(session2, "grep --help;ssh r732.pvt.bridges.psc.edu;");
         assertTrue(s2.contains("no active jobs on this node"));
         System.out.println(s2);
     }
@@ -45,10 +49,55 @@ public class sshServiceTest {
         s3.connect();
         s3.setPortForwardingL("localhost", 8086, "r014.pvt.bridges.psc.edu", 8086);
         System.err.println("Port forward set.");
-        // Stop 20s for local connect test
-        Thread.sleep(20000);
+        // Stop 1s for local connect test
+        Thread.sleep(1000);
         s3.delPortForwardingL(8086);
         s3.disconnect();
+    }
+
+    @Test
+    public void submitJobTest() throws JSchException, IOException {
+        Session s = generateNewSshSession();
+        String res = testRunner(s, "cd ~/dev;sbatch 1;");
+        s.disconnect();
+        Matcher m = jobIdPattern.matcher(res);
+        while (m.find()) {
+            System.err.println(String.format("JobID: <%s>", m.group()));
+        }
+        // Group count = 0 means one match or matched all
+        assertEquals(m.groupCount(), 0);
+    }
+
+    @Test
+    public void patternTest() {
+        Matcher m = jobIdPattern.matcher("Unable to allocate resources: Invalid");
+        while (m.find()) {
+            assertEquals(m.group(), "");
+        }
+    }
+
+    @Test
+    @Ignore
+    public void startRemotePscTest() throws InterruptedException {
+        InfluxSwitcherService iss = new InfluxSwitcherService();
+        if (iss.submitStartPscInflux()) {
+            while (iss.pscInfluxIsInQueue()) {
+                // Check every 5s to ensure that Influx is online
+                Thread.sleep(5000);
+            }
+            iss.tryInitRemoteInfluxHostname();
+            // InfluxDB takes over 5min to start
+            Thread.sleep(310 * 1000);
+            while (!iss.hasPscInfluxStarted()) {
+                // Check every 5s to ensure that Influx is available
+                Thread.sleep(5000);
+            }
+            iss.startPortForward();
+            // Stop 40 seconds for testing
+            Thread.sleep(40 * 1000);
+            iss.stopPortForward();
+            iss.stopPscInflux();
+        }
     }
 
     private String testRunner(Session session, String command) throws JSchException, IOException {
@@ -58,7 +107,7 @@ public class sshServiceTest {
         channel.setCommand(command);
 
         channel.setInputStream(null);
-        channel.setErrStream(System.err);
+        channel.setOutputStream(null);
         channel.setPty(true);
 
         InputStream in = channel.getInputStream();
@@ -78,7 +127,7 @@ public class sshServiceTest {
         // Change user name!
         Session s = jsch.getSession("xxx", InfluxappConfig.REMOTE_SSH_HOST);
         java.util.Properties config = new Properties();
-        config.put("StrictHostKeyChecking", "no");
+        config.put("StrictHostKeyChecking", "yes");
         config.put("PreferredAuthentications", "publickey");
         config.put("compression.s2c", "zlib,none");
         config.put("compression.c2s", "zlib,none");
