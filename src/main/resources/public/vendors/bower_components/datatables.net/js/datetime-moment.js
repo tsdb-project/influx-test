3f8c191289910d97c52b8fc52ca203b5d50015fb
isolachine <keanemu@gmail.com>
2019-05-12 06:16:37 -0400

    Update Framework

--- src/main/resources/public/vendors/bower_components/datatables.net/js/datetime-moment.js
+++ src/main/resources/public/vendors/bower_components/datatables.net/js/datetime-moment.js
@@ -1,16 +1,16 @@
 /**
  * This plug-in for DataTables represents the ultimate option in extensibility
  * for sorting date / time strings correctly. It uses
- * [Moment.js](http://momentjs.com) to create automatic type detection and
+ * Moment.js to create automatic type detection and
  * sorting plug-ins for DataTables based on a given format. This way, DataTables
  * will automatically detect your temporal information and sort it correctly.
  *
  * For usage instructions, please see the DataTables blog
- * post that [introduces it](//datatables.net/blog/2014-12-18).
+ * post that introduces it.
  *
  * @name Ultimate Date / Time sorting
  * @summary Sort date and time in any format using Moment.js
- * @author [Allan Jardine](//datatables.net)
+ * @author Allan Jardine
  * @depends DataTables 1.10+, Moment.js 1.7+
  *
  * @example
@@ -21,54 +21,54 @@
  */
 
 (function (factory) {
-	if (typeof define === "function" && define.amd) {
-		define(["jquery", "moment", "datatables.net"], factory);
-	} else {
-		factory(jQuery, moment);
-	}
+    if (typeof define === "function" && define.amd) {
+        define(["jquery", "moment", "datatables.net"], factory);
+    } else {
+        factory(jQuery, moment);
+    }
 }(function ($, moment) {
 
-$.fn.dataTable.moment = function ( format, locale ) {
-	var types = $.fn.dataTable.ext.type;
+    $.fn.dataTable.moment = function (format, locale) {
+        var types = $.fn.dataTable.ext.type;
 
-	// Add type detection
-	types.detect.unshift( function ( d ) {
-		if ( d ) {
-			// Strip HTML tags and newline characters if possible
-			if ( d.replace ) {
-				d = d.replace(/(<.*?>)|(\r?\n|\r)/g, '');
-			}
+        // Add type detection
+        types.detect.unshift(function (d) {
+            if (d) {
+                // Strip HTML tags and newline characters if possible
+                if (d.replace) {
+                    d = d.replace(/(<.*?>)|(\r?\n|\r)/g, '');
+                }
 
-			// Strip out surrounding white space
-			d = $.trim( d );
-		}
+                // Strip out surrounding white space
+                d = $.trim(d);
+            }
 
-		// Null and empty values are acceptable
-		if ( d === '' || d === null ) {
-			return 'moment-'+format;
-		}
+            // Null and empty values are acceptable
+            if (d === '' || d === null) {
+                return 'moment-' + format;
+            }
 
-		return moment( d, format, locale, true ).isValid() ?
-			'moment-'+format :
-			null;
-	} );
+            return moment(d, format, locale, true).isValid() ?
+                'moment-' + format :
+                null;
+        });
 
-	// Add sorting method - use an integer for the sorting
-	types.order[ 'moment-'+format+'-pre' ] = function ( d ) {
-		if ( d ) {
-			// Strip HTML tags and newline characters if possible
-			if ( d.replace ) {
-				d = d.replace(/(<.*?>)|(\r?\n|\r)/g, '');
-			}
+        // Add sorting method - use an integer for the sorting
+        types.order['moment-' + format + '-pre'] = function (d) {
+            if (d) {
+                // Strip HTML tags and newline characters if possible
+                if (d.replace) {
+                    d = d.replace(/(<.*?>)|(\r?\n|\r)/g, '');
+                }
 
-			// Strip out surrounding white space
-			d = $.trim( d );
-		}
-		
-		return !moment(d, format, locale, true).isValid() ?
-			Infinity :
-			parseInt( moment( d, format, locale, true ).format( 'x' ), 10 );
-	};
-};
+                // Strip out surrounding white space
+                d = $.trim(d);
+            }
+
+            return !moment(d, format, locale, true).isValid() ?
+                Infinity :
+                parseInt(moment(d, format, locale, true).format('x'), 10);
+        };
+    };
 
 }));
