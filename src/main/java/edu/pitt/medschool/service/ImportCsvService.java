c330f2f9809cc2a13728e394f74e6128bdb06fc1
Jasonliu-pitt <43303332+Jasonliu-pitt@users.noreply.github.com>
2019-06-25 13:50:43 -0400

    Update ImportCsvService.java

--- src/main/java/edu/pitt/medschool/service/ImportCsvService.java
+++ src/main/java/edu/pitt/medschool/service/ImportCsvService.java
@@ -335,7 +335,7 @@ public class ImportCsvService {
         String fileUUID = "";
 
         // fixing 3 hours time shift
-        double offset = -1 *1.0 / 24;
+//        double offset = -1 *1.0 / 24;
         try {
             BufferedReader reader = Files.newBufferedReader(file);
             CSVReader csvReader = new CSVReader(reader);
@@ -462,7 +462,7 @@ public class ImportCsvService {
                 }
 
                 // Compare date on every measures (They are all UTCs)
-                double sTime = Double.parseDouble(values[0]) + offset;
+                double sTime = Double.parseDouble(values[0]);
                 Date measurementDate = TimeUtil.serialTimeToDate(sTime, null);
                 long measurementEpoch = measurementDate.getTime();
 
