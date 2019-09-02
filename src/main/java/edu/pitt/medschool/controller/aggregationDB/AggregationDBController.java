862af911f0b161e04ef59a7629d7925213ffb037
Jasonliu-pitt <43303332+Jasonliu-pitt@users.noreply.github.com>
2019-09-02 16:11:08 -0400

    add front-end of the aggDatabase

--- src/main/java/edu/pitt/medschool/controller/aggregationDB/AggregationDBController.java
+++ src/main/java/edu/pitt/medschool/controller/aggregationDB/AggregationDBController.java
@@ -3,12 +3,23 @@ package edu.pitt.medschool.controller.aggregationDB;
 
 import edu.pitt.medschool.service.AggregationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class AggregationDBController {
 
     @Autowired
     AggregationService aggregationService;
+
+    @RequestMapping("/aggDatabase")
+    public Model aggDatabasePage(Model model) {
+        model.addAttribute("nav", "management");
+        model.addAttribute("subnav", "aggDatabase");
+        return model;
+    }
 
 }
