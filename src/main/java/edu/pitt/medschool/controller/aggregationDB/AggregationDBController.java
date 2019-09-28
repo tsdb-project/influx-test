aaeaa5678ea150ac8af973b1720e9034ad7f492e
Jasonliu-pitt <43303332+Jasonliu-pitt@users.noreply.github.com>
2019-09-28 17:26:33 -0400

    agg frontend complete

--- src/main/java/edu/pitt/medschool/controller/aggregationDB/AggregationDBController.java
+++ src/main/java/edu/pitt/medschool/controller/aggregationDB/AggregationDBController.java
@@ -33,13 +33,13 @@ public class AggregationDBController {
         model.addAttribute("subnav", "aggDatabase");
 //        List<String> trends = columnService.selectAllMeasures();
         List<String> trends = aggregationService.getColumns();
-//        List<AggregationDatabase> availableDatabases = aggregationService.selectAllAvailableDBs();
-//        List<String> DBNames = new ArrayList<>();
-//        for (AggregationDatabase database: availableDatabases){
-//            DBNames.add(database.getDbName());
-//        }
+        List<AggregationDatabase> availableDatabases = aggregationService.selectAllAvailableDBs();
+        List<String> DBNames = new ArrayList<>();
+        for (AggregationDatabase database: availableDatabases){
+            DBNames.add(database.getDbName());
+        }
         model.addAttribute("measures", trends);
-//        model.addAttribute("databases",DBNames);
+        model.addAttribute("databases",DBNames);
         return model;
     }
 
