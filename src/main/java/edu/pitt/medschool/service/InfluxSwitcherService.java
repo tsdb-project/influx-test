2ccc2b51b3e6da09df21b6d882dde057df1d87fb
Zhixiang Liu <43303332+Jasonliu-pitt@users.noreply.github.com>
2020-02-11 16:31:06 -0500

    Revert "change auto start script"
    This reverts commit 5a34a084ed0b85cb0aa45697a7781f027bbc820f.

--- src/main/java/edu/pitt/medschool/service/InfluxSwitcherService.java
+++ src/main/java/edu/pitt/medschool/service/InfluxSwitcherService.java
@@ -134,10 +134,9 @@ public class InfluxSwitcherService {
             return;
         }
         try {
-            String[] command = {"/bin/bash","/Volumes/INFLUX_RAID/config/startDB.sh"};
-            Runtime.getRuntime().exec(command);
-            // Local InfluxDB takes up to 5 mins for starting
-            Thread.sleep(300_000);
+            Runtime.getRuntime().exec(new String[]{"influxd -config /Volumes/INFLUX_RAID/config/load.ini"});
+            // Local InfluxDB takes up to 20s for starting
+            Thread.sleep(20_000);
         } catch (Exception e) {
             logger.error("Start local failed: {}", Util.stackTraceErrorToString(e));
             // Local may still started in this case
